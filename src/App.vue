a21fadec1098dec2a4b1abafb7b1134f78c14535
zhiwei.zha <zhazhiwei@rengu.cc>
2020-08-05 13:50:35 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>0</el-header>
+      <el-header>1</el-header>
+      <el-aside>2</el-aside>
+      <el-main>3</el-main>
+      <el-footer>4</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+import ElContainer from "element-ui/packages/container/src/main";
+import ElFooter from "element-ui/packages/footer/src/main";
+import ElHeader from "element-ui/packages/header/src/main"
+import ElAside from "element-ui/packages/aside/src/main"
+import ElMain from "element-ui/packages/main/src/main"
+
+export default {
+  name: 'App',
+  components: {
+      ElFooter,
+      ElContainer,
+      ElHeader,
+      ElAside,
+      ElMain
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/common/TopMenu.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "top-menu"
+    }
+</script>
+
+<style scoped>
+
+</style>
